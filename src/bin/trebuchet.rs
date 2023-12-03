6426938bba6f133b37a4b0b5858adc29232a6b15
Jake McGrath <thejakezilla80@gmail.com>
2023-12-03 17:19:45 -0600

    Fully completed day 1. She may not be beautiful, but she works.

--- src/bin/trebuchet.rs
+++ src/bin/trebuchet.rs
@@ -1,7 +1,26 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file_path = env::current_dir()
+        .expect("Unable to open cwd")
+        .join("assets")
+        .join("day_1_input.txt");
+
+    let file1 = File::open(&file_path).expect("Unable to open file");
+    let file2 = File::open(&file_path).expect("Unable to open file");
+
+    let reader1 = BufReader::new(file1);
+    let reader2 = BufReader::new(file2);
+
+    let part_one_sum = part_one(reader1);
+    let part_two_sum = part_two(reader2);
+
+    println!("Part One Solution:\t{}", part_one_sum);
+    println!("Part Two Solution:\t{}", part_two_sum);
+}
 
 /// # Day 1: Trebuchet?!
 /// Something is wrong with global snow production, and you've been selected to take a look.
@@ -43,24 +62,7 @@ use std::io::{BufRead, BufReader};
 /// Adding these together produces `142`.
 ///
 /// Consider your entire calibration document. *What is the sum of all of the calibration values?*
-fn main() {
-    let file_path = env::current_dir()
-        .expect("Unable to open cwd")
-        .join("assets")
-        .join("day_1_input.txt");
-    let file = File::open(file_path).expect("Unable to open file");
-
-    let reader1 = BufReader::new(&file);
-    let reader2 = BufReader::new(&file);
-
-    let part_one_sum = part_one(reader1);
-    let part_two_sum = part_two(reader2);
-
-    println!("Part One Solution:\t{}", part_one_sum);
-    println!("Part Two Solution:\t{}", part_two_sum);
-}
-
-fn part_one(reader: BufReader<&File>) -> usize {
+fn part_one(reader: BufReader<File>) -> usize {
     reader.lines()
         .filter_map(|line| line.ok())
         .filter_map(|line| {
@@ -76,6 +78,65 @@ fn part_one(reader: BufReader<&File>) -> usize {
         }).sum()
 }
 
-fn part_two(reader: BufReader<&File>) -> usize {
-    2
+/// Your calculation isn't quite right. It looks like some of the digits are actually *spelled out
+/// with letters:* `one`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, and `nine` *also*
+/// count as valid "digits".
+///
+/// Equipped with this new information, you now need to find the real first and last digit on each
+/// line.
+///
+/// # Example
+///
+/// ```
+/// two1nine
+/// eightwothree
+/// abcone2threexyz
+/// xtwone3four
+/// 4nineeightseven2
+/// zoneight234
+/// 7pqrstsixteen
+/// ```
+///
+/// In this example, the calibration values are `29`, `83`, `13`, `24`, `42`, `14`, and `76`. Adding
+/// these together produces `281`.
+///
+/// *What is the sum of all of the calibration values?*
+fn part_two(reader: BufReader<File>) -> usize {
+    let map_nums = vec![
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ];
+    let mut num_map: HashMap<&str, char> = HashMap::new();
+    for (k, v) in map_nums.into_iter() {
+        num_map.insert(k, v);
+    }
+
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .filter_map(|line| {
+            let mut v: VecDeque<char> = VecDeque::new();
+            for (i, c) in line.char_indices() {
+                if c.is_digit(10) {
+                    v.push_back(c);
+                } else if let substring = &line[i..line.len()] {
+                    for (key, val) in num_map.iter() {
+                        if substring.starts_with(key) {
+                            v.push_back(*val);
+                        }
+                    }
+                }
+            }
+            let first = v.pop_front().unwrap_or_default();
+            let last = v.pop_back().unwrap_or(first);
+            let concatenated = format!("{}{}", first, last);
+            concatenated.parse::<usize>().ok()
+        }).sum()
 }
