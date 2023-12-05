a3184fc2d3903e1048f4b63d57d9e1edc0693e72
Jake McGrath <jmcgra15@depaul.edu>
2023-12-05 00:01:12 -0600

    Completed day 2.

--- src/bin/cube_conundrum.rs
+++ src/bin/cube_conundrum.rs
@@ -1,8 +1,15 @@
-use std::collections::HashMap;
+use std::cmp::max;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use regex::Regex;
+
+#[derive(Debug)]
+struct _GameData {
+    _game_number: usize,
+    _color_counts: [usize; 3],  // [blue, green, red]
+    _is_valid: bool,
+}
 
 /// You're launching high into the atmosphere!  The apex of your trajectory just barely reaches the
 /// surface of a large island floating in the sky. You gently land in a fluffy pile of leaves. It's
@@ -50,30 +57,93 @@ use regex::Regex;
 /// Determine which games would have been possible if the bag had been loaded with only 12 red cubes,
 /// 13 green cubes, and 14 blue cubes. *What is the sum of the IDs of those games?*
 fn part_1(reader: BufReader<File>) -> usize {
-    let max_cube_counts: HashMap<&str, usize> = HashMap::from_iter(
-        vec![("red", 12), ("green", 13), ("blue", 14)]
-    );
-    let game_regex = Regex::new(r"Game (\d+):").expect("Unable to create game regex");
-    let color_regex = Regex::new(r"(\d+) (\w+)").expect("Unable to create color regex");
+    let color_regex = Regex::new(r"(\d+) (\w+)").expect("Bad color regex pattern");
 
-    let game_data: HashMap<usize, HashMap<String, usize>> = reader.lines()
+    reader
+        .lines()
         .filter_map(|line| line.ok())
-        .filter_map(|line| {
-            game_regex.captures(line.as_str()).and_then(|captures| {
-                // todo:
-            })
-        });
-
-    let temp = game_data
-        .iter()
-        .filter_map(|(game_num, color_count_map)| {
-            // TODO:
-        })
+        .enumerate()
+        .map(|(line_num, line)| {
+            let line = line.as_str();
+            let game_num = line_num + 1;
+            let has_valid_color_counts = color_regex
+                .captures_iter(line)
+                .map(|color_capture| {
+                    let count: usize = color_capture[1].parse().unwrap_or(0);
+                    match color_capture[2].to_string().as_str() {
+                        "blue" => count <= 14,
+                        "green" => count <= 13,
+                        "red" => count <= 12,
+                        _ => false,
+                    }
+                }).fold(true, |acc, new_count_valid| acc && new_count_valid);
+            if has_valid_color_counts {
+                game_num
+            } else {
+                0
+            }
+        }).sum()
 }
 
-fn part_2(_reader: BufReader<File>) -> usize {
-    2
-    // TODO
+/// The Elf says they've stopped producing snow because they aren't getting any *water*! He isn't
+/// sure why the water stopped; however, he can show you how to get the water source to check it out
+/// for yourself. It's just up ahead!
+///
+/// As you continue your walk, the Elf poses a second question: in each game you played, what is the
+/// *fewest number of cubes of each color* that could have been in the bag to make the game possible?
+///
+/// Again consider the example games from earlier:
+///
+/// # Example
+///
+/// ```
+/// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+/// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+/// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+/// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+/// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+/// ```
+///
+/// * In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If
+///   any color had been one fewer cube, the game would have been impossible.
+/// * Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+/// * Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+/// * Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+/// * Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+///
+/// The *power* of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied
+/// together. The power of the minimum set of cubes in game 1 is `48`. In games 2-5 it was `12`,
+/// `1560`, `630`, and `36`, respectively.  Adding up these five powers produces the sum `2286`.
+///
+/// For each game, find the minimum set of cubes that must have been present. *What is the sum of
+/// the power of these sets?*
+fn part_2(reader: BufReader<File>) -> usize {
+    let color_regex = Regex::new(r"(\d+) (\w+)").expect("Bad color regex pattern");
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            let line = line.as_str();
+
+            let (blue, green, red) = color_regex
+                .captures_iter(line)
+                .map(|color_capture| {
+                    let count: usize = color_capture[1].parse().unwrap_or(0);
+                    match color_capture[2].to_string().as_str() {
+                        "blue" => (count, 0, 0),
+                        "green" => (0, count, 0),
+                        "red" => (0, 0, count),
+                        _ => (0, 0, 0),
+                    }
+                }).fold((0, 0, 0), |acc, new_count| {
+                let b = max(acc.0, new_count.0);
+                let g = max(acc.1, new_count.1);
+                let r = max(acc.2, new_count.2);
+                (b, g, r)
+            });
+
+            blue * green * red
+        }).sum()
 }
 
 fn main() {
