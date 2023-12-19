752279f9776bf6ffe47bbfa770ae68763fc1d269
Jake McGrath <thejakezilla80@gmail.com>
2023-12-18 20:46:39 -0600

    Completed day 4.  Part 2 isn't pretty, but she works.

--- src/bin/scratchcards.rs
+++ src/bin/scratchcards.rs
@@ -59,43 +59,15 @@ use std::path::PathBuf;
 ///
 /// Take a seat in the large pile of colorful cards.  *How many points are they worth in total?*
 fn part_1(input: &str) -> usize {
-    let mut result = 0;
-    input
-        .lines()
-        .filter(|line| !line.is_empty())
-        .for_each(|line| {
-            let line = line.split(":").collect::<Vec<&str>>();
-            let line = match line.get(1) {
-                Some(l) => l.split("|").collect::<Vec<&str>>(),
-                None => Vec::default(),
-            };
-            let winning_nums: HashSet<i32> = match line.get(0) {
-                Some(nums_s) => nums_s.split_ascii_whitespace().filter_map(|s| s.parse::<i32>().ok()).collect(),
-                None => HashSet::default(),
-            };
-            let my_nums: Vec<i32> = match line.get(1) {
-                Some(nums_s) => nums_s.split_ascii_whitespace().filter_map(|s| s.parse::<i32>().ok()).collect(),
-                None => Vec::default(),
-            };
-            // If there's at least one matching value, then we can left shift our count (because we're doubling)
-            result += match my_nums.iter().filter(|num| winning_nums.contains(num)).count() {
-                matching_num_count if matching_num_count > 0 => 1 << (matching_num_count - 1),
-                _ => 0,
-            };
-        });
-    result
-}
-
-fn part_2(input: &str) -> usize {
     input
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| {
-            let parts: String = line.split(":")
-                .filter(|l| l.contains("|"))
+            let nums: Vec<&str> = line
+                .split(":")
+                .filter(|line| line.contains("|"))
+                .flat_map(|part| part.split("|"))
                 .collect();
-            let nums: Vec<&str> = parts.split("|").collect();
-
 
             let (winning_nums, my_nums): (HashSet<i32>, Vec<i32>) = match (nums.first(), nums.last()) {
                 (Some(w_n_s), Some( m_n_s)) => {
@@ -114,6 +86,82 @@ fn part_2(input: &str) -> usize {
         })
 }
 
+/// Just as you're about to report your findings to the Elf, one of you realizes that the rules have actually been
+/// printed on the back of every card this whole time.
+///
+/// There's no such thing as "points".  Instead, scratchcards only cause you to **win more scratchcards** equal to the
+/// number of winning numbers your have.
+///
+/// Specifically, you win **copies** of the scratchcards below the winning card equal to the number of matches.  So, if
+/// card 10 were to have 5 matching numbers, you would win one copy each of cards 11, 12, 13, 14, and 15.
+///
+/// Copies of scratchcards are scored like normal scratchcards and have the **same card number** as the card they copied.
+/// So, if you win a copy of card 10 and it has 5 matching numbers, it would then win a copy of the same cards that the
+/// original card 10 won: cards 11, 12, 13, 14, and 15.  This process repeats until none of the copies cause you to win
+/// any more cards.  (Cards will never make you copy a card past the end of the table.)
+///
+/// # Example:
+/// This time, the above example goes differently:
+/// ```
+/// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+/// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+/// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+/// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+/// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+/// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+/// ```
+/// * Card 1 has four matching numbers, so you win one copy each of the next four cards: cards 2, 3, 4, and 5.
+/// * Your original card 2 has two matching numbers, so you win one copy each of cards 3 and 4.
+/// * Your copy of card 2 also wins one copy each of cards 3 and 4.
+/// * Your four instances of card 3 (one original and three copies) have two matching numbers, so you win **four** copies
+///   each of cards 3 and 4.
+/// * Your four instances of card 3 (one original and three copies) have two matching numbers, so you win **eight** copies
+///   of card 5.
+/// * Your fourteen instances of card 5 (one original and thirteen copies) have no matching numbers and win no more cards.
+/// * Your one instance of card 6 (one original) has no matching numbers and wins no more cards.
+///
+/// Once all of the originals and copies have been processed, you end up with **1** instance of card 1, **2** instances of
+/// card 2, **4** instances of card 3, **8** instances of card 4, **14** instances of card 5, and **1** instance of card
+/// 6.  In total, this example pile of scratchcards causes you to ultimately have **30** scratchcards!
+///
+/// Process all of the original and copied scratchcards until no more scratchcards are won.  Including the original set
+/// of scratchcards, **how many total scratchcards do you end up with?**
+fn part_2(input: &str) -> usize {
+    let n = input.lines().count();
+    let mut copies: Vec<usize> = vec![1; n];
+
+    let cards: Vec<(HashSet<i32>, Vec<i32>)> = input.lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let nums: Vec<&str> = line
+                .split(":")
+                .filter(|line| line.contains("|"))
+                .flat_map(|part| part.split("|"))
+                .collect();
+            (
+                nums.first().unwrap().split_ascii_whitespace().filter_map(|n| n.parse().ok()).collect(),
+                nums.last().unwrap().split_ascii_whitespace().filter_map(|n| n.parse().ok()).collect(),
+                )
+        })
+        .collect();
+    for (index, (winning_nums, my_nums)) in cards.iter().enumerate() {
+        if let Some(copy_nums) = copies.get(index) {
+            let mut copy_nums = *copy_nums;
+            while copy_nums > 0 {
+                let mut i = index + 1;
+                let mut matches_count = my_nums.iter().filter(|n| winning_nums.contains(n)).count();
+                while matches_count > 0 {
+                    copies[i] += 1;
+                    i += 1;
+                    matches_count -= 1;
+                }
+                copy_nums -= 1;
+            }
+        }
+    }
+    copies.into_iter().sum()
+}
+
 fn main() {
     let cwd: PathBuf = env::current_dir().unwrap_or_default();
     let input_path: PathBuf = cwd.join("assets").join("day_4_input.txt");
@@ -126,6 +174,16 @@ fn main() {
     let part1_result: usize = part_1(part1_input);
     let part2_result: usize = part_2(part2_input);
 
+    let test_str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    let test_result: usize = part_2(test_str);
+
     println!("Part 1:\t{}", part1_result);
-    println!("Part 2:\t{}", part2_result);
+    println!("Part 2:\t{}", part2_result);      // 5132675
+    println!("Test:\t{}", test_result);
 }
